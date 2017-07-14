71fc3799208c1ff13b2850c80133b9f337f28d85
Fer Juarez <ferjuarez.lp@gmail.com>
2017-07-14 12:47:12 -0300

    Fixed wsgi.py

--- simplerest/wsgi.py
+++ simplerest/wsgi.py
@@ -8,8 +8,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplerest.settings")
 
 from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplerest.settings")
 from dj_static import Cling
 application = Cling(get_wsgi_application())
