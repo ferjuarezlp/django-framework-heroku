54085dc47b7fbe2378a124e388aca73c13fccdf3
Fer Juarez <ferjuarez.lp@gmail.com>
2017-07-14 11:51:43 -0300

    Initial Commit

---
+++ simplerest/api/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+    url(r'^$', 'simplerest.api.views.home'),
+    url(r'^list/', 'simplerest.api.views.api_list'),
+)
